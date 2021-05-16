788db72ed652d59d61af14319511ea030c970b73
Shadowbreaker GD <78216950+ShadowbreakerGD@users.noreply.github.com>
2021-05-16 10:28:26 -0400

    Create Show-Comment-Button.user.js

---
+++ Show-Comment-Button.user.js
@@ -0,0 +1,9 @@
+// ==UserScript==
+// @name        GDbrowser Show Comment button
+// @description A userscript that shows the comment button in the comments of levels that dont have the comment button. Mostly for commenting on nonexisting level ids
+// @match       *://gdbrowser.com/comments/*
+// @version 1.0
+// @namespace https://greasyfork.org/users/763612
+// ==/UserScript==
+
+$('#leaveComment').show()
